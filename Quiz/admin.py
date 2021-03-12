2b06ea5e387eb4ce01c96d59e020da4a803336a7
avnish ranwa <avnishranwa@jklu.edu.in>
2021-03-12 15:52:42 +0530

    commit on 'main'

--- Quiz/admin.py
+++ Quiz/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Course, enrolled, Student_data_insert
 from django.contrib.admin import ModelAdmin
+from django.contrib.contenttypes.models import ContentType
+from guardian.shortcuts import assign_perm
 # Register your models here.
 
 class EnrolledAdmin(admin.ModelAdmin):
@@ -9,7 +11,12 @@ class EnrolledAdmin(admin.ModelAdmin):
         ('Course_ID', admin.RelatedOnlyFieldListFilter),
     )
 
-
+content_type = ContentType.objects.get_for_model(Course)
+permission = Permission.objects.create(
+    codename='Course',
+    name='Can Change Course',
+    content_type=content_type,
+)
 
 admin.site.register(Course)
 admin.site.register(enrolled, EnrolledAdmin)
