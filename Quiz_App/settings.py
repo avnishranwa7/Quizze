220f0e8077557cbb598db97555219076af2ff67e
avnish ranwa <avnishranwa@jklu.edu.in>
2021-05-03 18:49:24 +0530

    final

--- Quiz_App/settings.py
+++ Quiz_App/settings.py
@@ -23,11 +23,11 @@ STATIC_URL = '/static/'
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'js35&#pawur#1(znr%w_p5i+6^@e#h49ev-5ku*3fzs7%!xp)z'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'js35&#pawur#1(znr%w_p5i+6^@e#h49ev-5ku*3fzs7%!xp)z')
+SECRET_KEY = 'js35&#pawur#1(znr%w_p5i+6^@e#h49ev-5ku*3fzs7%!xp)z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = True
+
 ALLOWED_HOSTS = []
 
 
